e6cace8d1d68974bce72e42fc1b43babbedbb416
Ashleigh <ashleighmaund@gmail.com>
2015-06-02 19:04:51 +0100

    committing and creating a new branch to experiment a bit

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -21,7 +21,8 @@ describe('Bowling', function() {
 
 	it('will only let player input scores for ten frames', function() {
 		for (var i=1; i<10; i++) {
-		bowling.addPointsToFrame(3,3)};
+			bowling.addPointsToFrame(3,3)
+		};
 		expect(bowling.howManyFramesNow).toEqual(10);
 	});
 
@@ -52,6 +53,64 @@ describe('Bowling', function() {
 		bowling.addPointsToFrame(6,4);
 		bowling.addPointsToFrame(7,2);
 		expect(bowling.scorecard).toEqual(26);
+	});
+
+	it('can calculate the bonus score when player has rolled a spare', function() {
+		bowling.addPointsToFrame(6,4);
+		bowling.addPointsToFrame(2,2);
+		expect(bowling.scorecard).toEqual(16);
+	});
+
+	it('can calculate the bonus score when player has rolled a strike', function() {
+		bowling.addPointsToFrame(10,0);
+		bowling.addPointsToFrame(7,2);
+		expect(bowling.scorecard).toEqual(28);
+	});
+
+	it('can calculate the bonus score when player has rolled two strikes in a row', function() {
+		bowling.addPointsToFrame(2,4);
+		bowling.addPointsToFrame(10,0);
+		bowling.addPointsToFrame(10,0);
+		bowling.addPointsToFrame(7,2);
+		expect(bowling.scorecard).toEqual(61);
+	});
+
+	// HERE!!!
+
+	// [[10,0],[10,0],[7,2]]
+
+	// 10 + (10 + 7) = 27
+	// 10 + (7 + 2) = 19
+	// 7 + 2 = 9
+	// 55
+
+	// 10 + 10 + 7
+	// 10 + 7 + 2
+	// 7 + 2
+
+
+	// 27 + 19 + 9
+	// 46 + 9
+	// 55
+
+	it('can calculate the bonus score when player has rolled two strikes in a row', function() {
+		bowling.addPointsToFrame(2,4);
+		bowling.addPointsToFrame(10,0);
+		bowling.addPointsToFrame(10,0);
+		bowling.addPointsToFrame(3,4);
+		expect(bowling.scorecard).toEqual(53);
+	});
+
+	// 10 + (10 + 3) = 23
+	// 10 + (3 + 4) = 17
+	// 3 + 4 = 7
+	// 47
+
+	xit('can calculate the score when a player rolls a perfect game', function() {
+		for (var i=1; i<11; i++) {
+			bowling.addPointsToFrame(10,0);
+		};
+		expect(bowling.scorecard).toEqual(300);
 	});
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -10,37 +10,75 @@ function Bowling() {
 	};
 
 	Bowling.prototype.addPointsToFrame = function(firstpoint, secondpoint) {
-		if ((this.howManyFramesNow > 1) && (this.bonusScoreForSpare())) {
+		if ((this.howManyFramesNow > 1) && (this.conditionForStrike())) {
+			this.bonusScoreForStrike(firstpoint, secondpoint);
+		};
+		if ((this.howManyFramesNow > 1) && (this.conditionForSpare()) && (this.conditionForStrike() === false)) {
 			this.addPointsToScorecard(firstpoint);
 		};
 		if ((firstpoint < 10) && (this.isMoreThanTen(firstpoint, secondpoint))) {
 			this.addPointsToScorecard(firstpoint);
 		};
 		if (this.isTenOrLess(firstpoint, secondpoint)) {
-			this.addPointsToScorecard(firstpoint);
-			this.addPointsToScorecard(secondpoint);
-			this.moveToNextFrame();
-			(this.framepoints).push([firstpoint, secondpoint]);
+			this.noBonusScoreToAdd(firstpoint,secondpoint);
 		};
+	};
+
+	Bowling.prototype.noBonusScoreToAdd = function(firstpoint,secondpoint) {
+		this.addPointsToScorecard(firstpoint);
+		this.addPointsToScorecard(secondpoint);
+		this.moveToNextFrame();
+		(this.framepoints).push([firstpoint, secondpoint]);
+	};
+
+	Bowling.prototype.bonusScoreForStrike = function(firstpoint, secondpoint) {
+		var currentFrameIndex = (this.framepoints).length;
+		var frameBeforeCurrent = this.framepoints[currentFrameIndex-1];
+		this.addPointsToScorecard(firstpoint);
+		this.addPointsToScorecard(secondpoint);
+		if (currentFrameIndex[1] === 0) {
+			this.addPointsToScorecard(firstpoint);
+		};
+
+		// if ((frameBeforeCurrent[0]===10)) {
+		// 	this.addPointsToScorecard(firstpoint);
+		// 	this.noBonusScoreToAdd(firstpoint, secondpoint);
+		// } else {
+		// 	this.addPointsToScorecard(firstpoint);
+		// };
+
+
+// if frameBeforeCurrent[1] === 0
+// 	then add currentFrame[0]
+// else
+// 	var sumOfFrame = firstpoint + secondpoint
+// 	add the next two addPointsToScorecard(sumOfFrame)
+
 	};
 
 	Bowling.prototype.moveToNextFrame = function() {
 		this.howManyFramesNow = this.howManyFramesNow + 1;
 	};
 
-	Bowling.prototype.bonusScoreForSpare = function() {
+	Bowling.prototype.conditionForStrike = function() {
 		var frameBeforeCurrentIndex = (this.framepoints).length -1;
 		var frameBeforeCurrent = this.framepoints[frameBeforeCurrentIndex];
-		var sumOfEachFrame = 0
+		return (frameBeforeCurrent[0] === 10);
+	};
+
+	Bowling.prototype.conditionForSpare = function() {
+		var frameBeforeCurrentIndex = (this.framepoints).length -1;
+		var frameBeforeCurrent = this.framepoints[frameBeforeCurrentIndex];
+		var sumOfEachFrame = 0;
 		for (var i=0; i<2; ++i) {
 			sumOfEachFrame += frameBeforeCurrent[i];
 		};	
 		return (sumOfEachFrame === 10);
 	};
 
-	// Bowling.prototype.sumOfEachFrame = function(firstpoint, secondpoint) {
-	// 	var number = firstpoint + secondpoint;
-	// };
+	Bowling.prototype.sumOfFrame = function(firstpoint, secondpoint) {
+		var number = firstpoint + secondpoint;
+	};
 
 	Bowling.prototype.isTenOrLess = function(firstpoint, secondpoint) {
 		return (firstpoint + secondpoint) <= 10;
